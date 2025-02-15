e8acef13db3ea9215c3aebf36ea506aca95d41c8
Paulina-Test <naruchan1410@gmail.com>
2025-02-15 22:56:30 +0000

    Updated average program to display min and max

--- average.py
+++ average.py
@@ -1,3 +1,6 @@
 numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
 avg = sum(numbers) / len(numbers)
 print("Average:", avg)
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))
+
